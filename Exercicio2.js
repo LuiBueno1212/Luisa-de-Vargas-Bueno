4ca8c52abe150ddced2dd1d806672431b3641c92
LuiBueno1212 <luisadevargasbueno@gmail.com>
2024-08-30 14:26:44 -0300

    Add files via upload

---
+++ Exercicio2.js
@@ -0,0 +1,12 @@
+//Crie uma variavel que receba e armazene a sua idade no tipo number
+let idade = 15
+
+//Crie outra variavel que receba o valor da sua primeira variavel, mas com o seu tipo alterado para string
+let idade2 = idade.toString()
+
+//Imprima duas mensagens no terminal
+//A primeira deve mostrar o valor e o tipo da primeira variavel
+console.log (idade, typeof idade)
+
+//A segunda deve mostrar o valor e o tipo da segunda variavel
+console.log (idade2, typeof idade2)
